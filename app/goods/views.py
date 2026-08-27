ac897899ccdadc0e8a5540174f0df37b798b3f6b
Tim <tim.ca2023@gmail.com>
2026-08-27 18:43:36 -0400

    added pagination

--- app/goods/views.py
+++ app/goods/views.py
@@ -1,16 +1,21 @@
-from django.shortcuts import render, get_object_or_404
+from django.shortcuts import render, get_list_or_404
+from django.core.paginator import Paginator
 from .models import Product, Category
 
 
-def catalog(request, category_slug):
+def catalog(request, category_slug, page=1):
     if category_slug == "all":
         goods = Product.objects.all()
     else:
-        goods = get_object_or_404(Product.objects.filter(category__slug=category_slug))
+        goods = get_list_or_404(Product.objects.filter(category__slug=category_slug))
+
+    paginator = Paginator(goods, 3)
+    current_page = paginator.page(page)
 
     context = {
         "title": "Home - Catalog",
-        "goods": goods,
+        "goods": current_page,
+        "slug_url": category_slug,
     }
 
     return render(request, "goods/catalog.html", context)

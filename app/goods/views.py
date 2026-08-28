b9cf9a0885b32605e17bc8e2867f4e4cdacf301f
Tim <tim.ca2023@gmail.com>
2026-08-28 18:04:40 -0400

    second pagination option - get params

--- app/goods/views.py
+++ app/goods/views.py
@@ -3,14 +3,16 @@ from django.core.paginator import Paginator
 from .models import Product, Category
 
 
-def catalog(request, category_slug, page=1):
+def catalog(request, category_slug):
+    page = request.GET.get("page", 1) # Get the current page number if provided, if no page then default to 1
+
     if category_slug == "all":
         goods = Product.objects.all()
     else:
         goods = get_list_or_404(Product.objects.filter(category__slug=category_slug))
 
     paginator = Paginator(goods, 3)
-    current_page = paginator.page(page)
+    current_page = paginator.page(int(page))
 
     context = {
         "title": "Home - Catalog",

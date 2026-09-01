1fee5f7c49435305b13f671325579b87a30c9c71
Tim <tim.ca2023@gmail.com>
2026-09-01 18:00:49 -0400

    the product filter and urlencode

--- app/goods/templatetags/goods_tags.py
+++ app/goods/templatetags/goods_tags.py
@@ -1,5 +1,6 @@
-from goods.models import Category
+from django.utils.http import urlencode
 from django import template
+from goods.models import Category
 
 register = template.Library()
 
@@ -7,3 +8,10 @@ register = template.Library()
 @register.simple_tag()
 def tag_category():
     return Category.objects.all()
+
+
+@register.simple_tag(takes_context=True)
+def change_params(context, **kwargs):
+    query = context["request"].GET.dict()
+    query.update(kwargs)
+    return urlencode(query)
